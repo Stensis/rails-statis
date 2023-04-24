19461c6117cb58c6fd3aa1c60b422a5c29eeaaa5
Stensis <irenenjuguna98@gmail.com>
2023-04-24 14:51:09 +0300

    added micropost controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,34 @@
 class MicropostsController < ApplicationController
+        # used to render pages 
+    # to add all microposts
+    def index
+        @microposts = Micropost.all
+        # render json: @microposts
+    end
+
+    # to show a micropost by id
+    def show
+    end
+
+    # to add a new micropost 
+    def new
+    end
+
+    # to update already stored info in the database by id
+    def edit
+    end
+    
+    # =====this are not supposed to be seen in the views erb communicates to the db only =========
+
+    # to create a new micropost
+    def create
+    end
+
+    # used to update info in db by id
+    def update
+    end
+
+    #to delete a micropost in db  by id
+    def destroy
+    end
 end
