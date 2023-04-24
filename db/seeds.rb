a9f5ec3ef15e4b2d8a90d558e66b9cd940d8878f
Stensis <irenenjuguna98@gmail.com>
2023-04-24 14:48:42 +0300

    added rails migration and seed file for microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,23 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-users = [
-    { name: "Robert Wilson", email: "robert.wilson@example.com" },
-    { name: "Melissa Brown", email: "melissa.brown@example.com" },
-    { name: "William Garcia", email: "william.garcia@example.com" }
-  ]
-  
-  users.each do |user|
-    User.create!(user)
-  end
-  
+    users = [
+        { name: "Robert Wilson", email: "robert.wilson@example.com" },
+        { name: "Melissa Brown", email: "melissa.brown@example.com" },
+        { name: "William Garcia", email: "william.garcia@example.com" }
+    ]
+    
+    users.each do |user|
+        User.create!(user)
+    end
+    
+
+    # Create a micropost without an associated user
+    micropost = Micropost.create(text: "This is an example micropost with user 1.",user_id: 1)
+    micropost = Micropost.create(text: "This is an example micropost with user 2.",user_id: 2)
+    micropost = Micropost.create(text: "This is an example micropost with user 3.",user_id: 3)
+
+
+    # Output a message to confirm that the seed was created
+    puts "Created micropost with text '#{micropost.text}'."
+
