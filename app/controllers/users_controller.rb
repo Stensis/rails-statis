d030696aca24bb49b2c9bda32e8f6533a1f09dd3
Stensis <irenenjuguna98@gmail.com>
2023-04-24 14:39:12 +0300

    added user index action and its index.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,35 @@
 class UsersController < ApplicationController
-    # to
+    # used to render pages 
+    # to add all users
     def index
+        @users = User.all
+        # render json: @users
     end
 
+    # to show a user by id
+    def show
+    end
+
+    # to add a new user 
+    def new
+    end
+
+    # to update already stored info in the database by id
+    def edit
+    end
+    
+    # =====this are not supposed to be seen in the views erb communicates to the db only =========
+
+    # to create a new user
+    def create
+    end
+
+    # used to update info in db by id
+    def update
+    end
+
+    #to delete a user in db  by id
+    def destroy
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = [
+    { name: "Robert Wilson", email: "robert.wilson@example.com" },
+    { name: "Melissa Brown", email: "melissa.brown@example.com" },
+    { name: "William Garcia", email: "william.garcia@example.com" }
+  ]
+  
+  users.each do |user|
+    User.create!(user)
+  end
+  
